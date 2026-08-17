5dddc7675bbfe0be02ccc135a81ff2f00f77ef33
Essam <essamhisham402@gmail.com>
2026-08-18 00:05:24 +0300

    HIDE

--- src/config/features.config.ts
+++ src/config/features.config.ts
@@ -95,7 +95,6 @@ export const enabledFeatures: FeatureId[] = [
   "premium-dfy-profit",
   "premium-autopilot",
   "protector",
-  "premium-license-rights",
 ];
 
 export function isFeatureEnabled(id: FeatureId): boolean {
